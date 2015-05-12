52e5a4c70757dc58ad0ee5b07dca8a766a53ae8e
Rebecca Kehl <bcckehl@gmail.com>
2015-05-12 15:28:42 -0500

    Worked on DBDriver.

---
+++ src/CatPhoto.java
@@ -0,0 +1,102 @@
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by rebeccakehl on 5/12/15.
+ */
+public class CatPhoto {
+
+    private String id;
+    @SerializedName("_id")
+    private String photoDataId;
+    @SerializedName("width")
+    int width = 0;
+    @SerializedName("height")
+    int height = 0;
+    @SerializedName("noseX")
+    int noseX = 0;
+    @SerializedName("noseY")
+    int noseY = 0;
+    @SerializedName("xShift")
+    int xShift = 0;
+    @SerializedName("yShift")
+    int yShift = 0;
+    @SerializedName("URL")
+    String URL;
+
+    public CatPhoto(String id, String URL) {
+        this.id = id;
+        this.URL = URL;
+    }
+
+    public String getPhotoDataId() {
+        return photoDataId;
+    }
+
+    public void setPhotoDataId(String photoDataId) {
+        this.photoDataId = photoDataId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getNoseX() {
+        return noseX;
+    }
+
+    public void setNoseX(int noseX) {
+        this.noseX = noseX;
+    }
+
+    public int getNoseY() {
+        return noseY;
+    }
+
+    public void setNoseY(int noseY) {
+        this.noseY = noseY;
+    }
+
+    public int getxShift() {
+        return xShift;
+    }
+
+    public void setxShift(int xShift) {
+        this.xShift = xShift;
+    }
+
+    public int getyShift() {
+        return yShift;
+    }
+
+    public void setyShift(int yShift) {
+        this.yShift = yShift;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+}
--- src/DBDriver.java
+++ src/DBDriver.java
@@ -1,14 +1,65 @@
 /**
  * Created by rebeccakehl on 5/12/15.
  */
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import static com.mongodb.client.model.Filters.*;
+
+import jdk.nashorn.internal.parser.JSONParser;
 import org.bson.Document;
-import org.bson.json.JsonReader;
+
+import java.util.ArrayList;
 
 public class DBDriver {
     private MongoCollection<Document> photoData;
     private MongoCollection<Document> catPhotos;
+
+    public DBDriver() {
+        MongoClient mongoClient = new MongoClient("localhost");
+
+        MongoDatabase database = mongoClient.getDatabase("");
+
+        photoData = database.getCollection("");
+        catPhotos = database.getCollection("");
+    }
+
+    public CatPhoto getCat(int noseX, int noseY) {
+        //Nearest neighbor search on photo data for noseX and noseY
+
+        Document catPhoto = photoData.find().first();
+
+
+        return null;
+    }
+
+    public boolean insertCat(int width, int height, int noseX, int noseY, String URL) {
+        ArrayList<Integer> noseArray = new ArrayList<>();
+        noseArray.add(noseX);
+        noseArray.add(noseY);
+        Document photoDoc = new Document("width", width)
+                .append("height", height)
+                .append("oldNose", noseArray)
+                .append("URL", URL);
+
+        catPhotos.insertOne(photoDoc);
+
+        Document photoDocFromMongo = catPhotos.find(eq("URL", URL)).first();
+        String photoString = photoDocFromMongo.toJson();
+        Gson gson = new Gson();
+        CatPhoto catPhoto = gson.fromJson(photoString, CatPhoto.class);
+
+        Document dataDoc = new Document("photoId", catPhoto.getPhotoDataId())
+                .append("xShift", 0)
+                .append("yShift", 0)
+                .append("nose", noseArray);
+
+        photoData.insertOne(dataDoc);
+
+        return false;
+    }
 }
